c94fe2d9e8e2c1bab6058933ff467e88a661dd6f
leejaelll <jayaom91076@gmail.com>
2023-05-15 23:31:05 +0900

    feat: Create ProductCard Component

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -6,6 +6,9 @@ import { useState } from 'react';
 import bookmarkOff from '../assets/bookmarkOff.svg';
 import bookmarkOn from '../assets/bookmarkOn.svg';
 
+import addBookmarkProducts from '../utils/addBookmarkProducts';
+import removeBookmarkProducts from '../utils/removeBookmarkProducts';
+
 const CardImg = styled.div`
   width: 100%;
   height: 210px;
@@ -45,17 +48,13 @@ const RightSection = styled.div`
   flex-direction: column;
   text-align: right;
 
-  strong {
+  div {
     font-weight: 600;
     margin-bottom: 0.25rem;
   }
 `;
 
-export default function ProductCard({
-  list,
-  bookmark,
-  handleClickAddBookmark,
-}) {
+export default function ProductCard({ list, bookmark }) {
   const {
     type,
     title,
@@ -70,16 +69,16 @@ export default function ProductCard({
 
   const [isBooked, setIsBooked] = useState(bookmark);
 
-  const handleClick = () => {
+  const handleClick = (list) => {
     setIsBooked((isBooked) => !isBooked);
-    !bookmark && handleClickAddBookmark(list);
+    isBooked ? removeBookmarkProducts(list) : addBookmarkProducts(list);
   };
 
   return (
     <GridItem>
       <CardImg>
         <img src={image_url || brand_image_url} />
-        <BookmarkStar onClick={handleClick}>
+        <BookmarkStar onClick={() => handleClick(list)}>
           {isBooked ? <img src={bookmarkOn} /> : <img src={bookmarkOff} />}
         </BookmarkStar>
       </CardImg>
@@ -92,8 +91,8 @@ export default function ProductCard({
           {sub_title && <h3>{sub_title}</h3>}
         </LeftSection>
         <RightSection>
-          {(type === 'Product' && <strong>{discountPercentage}%</strong>) ||
-            (type === 'Brand' && <strong>관심고객수</strong>)}
+          {(type === 'Product' && <div>{discountPercentage}%</div>) ||
+            (type === 'Brand' && <div>관심고객수</div>)}
           {(type === 'Product' && (
             <span>{Number(price).toLocaleString()}</span>
           )) ||
--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,20 +1,23 @@
-import axios from 'axios';
-
-import { useState, useEffect } from 'react';
-
 import styled from 'styled-components';
 import { GridContainer } from '../styles/GridStyle';
 
 import ProductCard from '../components/ProductCard';
 
+import useFetchProductListProducts from '../hooks/useFetchProductListProducts';
+import useFetchBookmarkProducts from '../hooks/useFetchBookmarkProducts';
+
+import checkBookmarkedItem from '../utils/checkBookmarkedItem';
+
 const ProductList = styled.div`
   display: flex;
   flex-direction: column;
+  padding: 24px 76px;
 `;
 
 const Title = styled.h1`
   font-size: 1.5rem;
   font-weight: 700;
+  margin-bottom: 12px;
 `;
 
 const BookmarkList = styled.div`
@@ -23,64 +26,18 @@ const BookmarkList = styled.div`
 `;
 
 export default function MainPage() {
-  const [productList, setProductList] = useState([]);
-  const [bookmarkItem, setBookmarkItem] = useState([]);
-
-  useEffect(() => {
-    (async () => {
-      try {
-        const response = await axios.get(
-          'http://cozshopping.codestates-seb.link/api/v1/products?count=4'
-        );
-        const data = response.data;
-        setProductList(data);
-
-        const bookmarkData = JSON.parse(localStorage.getItem('bookmarkedItem'));
-        if (bookmarkData === null) {
-          localStorage.setItem('bookmarkedItem', JSON.stringify([]));
-        }
-        setBookmarkItem(bookmarkData);
-      } catch {
-        console.log('Error');
-      }
-    })();
-  }, []);
-
-  const handleClickAddBookmark = (product) => {
-    // localStorage가 없으면 localStorage부터 만들기
-    if (localStorage.getItem('bookmarkedItem') === null) {
-      localStorage.setItem('bookmarkedItem', JSON.stringify([product]));
-    } else {
-      // localStorage가 있으면 localStorage에 해당 리스트 정보를 집어넣기
-      const prevItem = JSON.parse(localStorage.getItem('bookmarkedItem'));
-      localStorage.setItem(
-        'bookmarkedItem',
-        JSON.stringify([...prevItem, product])
-      );
-    }
-  };
-
-  const handleClickRemoveBookmark = (product) => {
-    // remove는 무조건 배열이 있을 것
-    const Item = JSON.parse(localStorage.getItem('bookmarkedItem'));
-  };
-
-  const bookmarkedItemArray = bookmarkItem.map((item) => item.id);
+  const products = useFetchProductListProducts();
+  const bookmarkProducts = useFetchBookmarkProducts();
 
   return (
     <div>
       <ProductList>
         <Title>상품 리스트</Title>
         <GridContainer>
-          {productList.map((list) => {
-            const isBooked = bookmarkedItemArray.includes(list.id);
+          {products.map((list) => {
+            const isBooked = checkBookmarkedItem(bookmarkProducts, list);
             return (
-              <ProductCard
-                key={list.id}
-                list={list}
-                bookmark={isBooked}
-                handleClickAddBookmark={handleClickAddBookmark}
-              />
+              <ProductCard key={list.id} list={list} bookmark={isBooked} />
             );
           })}
         </GridContainer>
