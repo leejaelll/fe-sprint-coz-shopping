27ae4384b5ee71fa30be088d0b969a062751ee98
leejaelll <jayaom91076@gmail.com>
2023-05-16 12:27:58 +0900

    feat: Create ProductListCategory Compoent

--- src/pages/ProductListPage.js
+++ src/pages/ProductListPage.js
@@ -1,3 +1,35 @@
+import { GridContainer } from '../styles/GridStyle';
+
+import { useState } from 'react';
+
+import ProductListCategory from '../components/ProductListCategory';
+
+import useFetchAllProducts from '../hooks/useFetchAllProducts';
+
+import selectTypes from '../utils/selectTypes';
+
 export default function ProductListPage() {
-  return <h1>ProductListPage</h1>;
+  const products = useFetchAllProducts();
+  const productTypes = selectTypes(products);
+
+  const [type, setType] = useState('Total');
+
+  const handleClickChangeType = (type) => {
+    setType(type);
+  };
+  return (
+    <div>
+      <ProductListCategory
+        activeType={type}
+        productTypes={productTypes}
+        handleClickChangeType={handleClickChangeType}
+      />
+      <GridContainer>
+        {/* {products.map((list) => {
+          const isBooked = checkBookmarkedItem(bookmarkProducts, list);
+          return <ProductCard key={list.id} list={list} bookmark={isBooked} />;
+        })} */}
+      </GridContainer>
+    </div>
+  );
 }
