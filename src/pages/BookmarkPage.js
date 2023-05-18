e032dff55992f5162ca45145b9c6466a6ef4fbea
leejaelll <jayaom91076@gmail.com>
2023-05-18 11:51:54 +0900

    feat: Create Toast modal UI
    - 북마크 버튼을 클릭했을 때, 북마크가 추가 혹은 제거되었다는 토스트 모달창 구현

--- src/pages/BookmarkPage.js
+++ src/pages/BookmarkPage.js
@@ -1,20 +1,20 @@
+import { useState } from 'react';
+
+import styled from 'styled-components';
 import { GridContainer } from '../styles/GridStyle';
 
-import { useState } from 'react';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { addNotify, removeNotify } from '../utils/toastNotify';
 
 import ProductListCategory from '../components/ProductListCategory';
 import ProductCard from '../components/ProductCard';
 
 import useFetchAllProducts from '../hooks/useFetchAllProducts';
+import useFetchBookmarkProducts from '../hooks/useFetchBookmarkProducts';
 
 import selectTypes from '../utils/selectTypes';
 import checkBookmarkedItem from '../utils/checkBookmarkedItem';
-import useFetchBookmarkProducts from '../hooks/useFetchBookmarkProducts';
-import styled from 'styled-components';
-
-const Container = styled.div`
-  padding: 0 76px;
-`;
 
 export default function BookmarkPage() {
   const products = useFetchAllProducts();
@@ -25,6 +25,8 @@ export default function BookmarkPage() {
   const handleClickChangeType = (type) => {
     setType(type);
   };
+
+  const [toastMessage, setToastMessage] = useState({ id: 0, message: '' });
 
   const filteredProducts =
     type === 'Total'
@@ -41,9 +43,29 @@ export default function BookmarkPage() {
       <GridContainer>
         {filteredProducts.map((list) => {
           const isBooked = checkBookmarkedItem(bookmarkProducts, list);
-          return <ProductCard key={list.id} list={list} bookmark={isBooked} />;
+          return (
+            <ProductCard
+              key={list.id}
+              list={list}
+              bookmark={isBooked}
+              setToastMessage={setToastMessage}
+              addNotify={addNotify}
+              removeNotify={removeNotify}
+            />
+          );
         })}
       </GridContainer>
+      {toastMessage.message && (
+        <ToastContainer
+          position="bottom-right"
+          autoClose={2000}
+          hideProgressBar={true}
+        />
+      )}
     </Container>
   );
 }
+
+const Container = styled.div`
+  padding: 0 76px;
+`;
--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,12 +1,77 @@
 import styled from 'styled-components';
 import { GridContainer } from '../styles/GridStyle';
 
+import { useState } from 'react';
+
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { addNotify, removeNotify } from '../utils/toastNotify';
+
 import ProductCard from '../components/ProductCard';
 
 import useFetchProductListProducts from '../hooks/useFetchProductListProducts';
 import useFetchBookmarkProducts from '../hooks/useFetchBookmarkProducts';
 
 import checkBookmarkedItem from '../utils/checkBookmarkedItem';
+
+export default function MainPage() {
+  const products = useFetchProductListProducts();
+  const bookmarkProducts = useFetchBookmarkProducts();
+  const [toastMessage, setToastMessage] = useState({ id: 0, message: '' });
+
+  return (
+    <div>
+      <ProductList>
+        <Title>상품 리스트</Title>
+        <GridContainer>
+          {products.map((list) => {
+            const isBooked = checkBookmarkedItem(bookmarkProducts, list);
+            return (
+              <ProductCard
+                key={list.id}
+                list={list}
+                bookmark={isBooked}
+                setToastMessage={setToastMessage}
+                addNotify={addNotify}
+                removeNotify={removeNotify}
+              />
+            );
+          })}
+        </GridContainer>
+      </ProductList>
+      <BookmarkList>
+        <Title>북마크 리스트</Title>
+        <GridContainer>
+          {bookmarkProducts.length === 0 ? (
+            <GridItemCol12>북마크 리스트가 비어있습니다.🦁</GridItemCol12>
+          ) : (
+            bookmarkProducts.map((list) => {
+              const isBooked = checkBookmarkedItem(bookmarkProducts, list);
+              return (
+                <ProductCard
+                  key={list.id}
+                  list={list}
+                  bookmark={isBooked}
+                  setToastMessage={setToastMessage}
+                  addNotify={addNotify}
+                  removeNotify={removeNotify}
+                />
+              );
+            })
+          )}
+        </GridContainer>
+      </BookmarkList>
+
+      {toastMessage.message && (
+        <ToastContainer
+          position="bottom-right"
+          autoClose={2000}
+          hideProgressBar={true}
+        />
+      )}
+    </div>
+  );
+}
 
 const ProductList = styled.div`
   display: flex;
@@ -30,39 +95,3 @@ const GridItemCol12 = styled.div`
   margin-top: 1rem;
   grid-column: span 12;
 `;
-
-export default function MainPage() {
-  const products = useFetchProductListProducts();
-  const bookmarkProducts = useFetchBookmarkProducts();
-
-  return (
-    <div>
-      <ProductList>
-        <Title>상품 리스트</Title>
-        <GridContainer>
-          {products.map((list) => {
-            const isBooked = checkBookmarkedItem(bookmarkProducts, list);
-            return (
-              <ProductCard key={list.id} list={list} bookmark={isBooked} />
-            );
-          })}
-        </GridContainer>
-      </ProductList>
-      <BookmarkList>
-        <Title>북마크 리스트</Title>
-        <GridContainer>
-          {bookmarkProducts.length === 0 ? (
-            <GridItemCol12>북마크 리스트가 비어있습니다.🦁</GridItemCol12>
-          ) : (
-            bookmarkProducts.map((list) => {
-              const isBooked = checkBookmarkedItem(bookmarkProducts, list);
-              return (
-                <ProductCard key={list.id} list={list} bookmark={isBooked} />
-              );
-            })
-          )}
-        </GridContainer>
-      </BookmarkList>
-    </div>
-  );
-}
